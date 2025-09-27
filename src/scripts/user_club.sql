e1af4a64f138e971274acbf852c3faa27acb9d92
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2025-09-27 17:08:45 -0400

    Add composite primary key of user ID and club ID

--- src/scripts/user_club.sql
+++ src/scripts/user_club.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS UserClub (
     user_id integer NOT NULL REFERENCES AppUser(id),
-    club_id integer NOT NULL REFERENCES Club(id)
+    club_id integer NOT NULL REFERENCES Club(id),
+    PRIMARY KEY(user_id, club_id)
 );
