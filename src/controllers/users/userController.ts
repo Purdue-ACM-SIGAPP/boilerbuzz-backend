8eb0d62111d62111d6c6765645aef8c344dcaafa
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2025-09-30 20:05:31 -0400

    Separate into controller and routes

---
+++ src/controllers/users/userController.ts
@@ -0,0 +1,69 @@
+import { Router, Request, Response } from "express";
+
+import pool from "../libs/db";
+
+export const listUsers = async (_request: Request, response: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM Users");
+    console.log("Get list of users from database");
+    response.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Failed to list users" });
+  }
+};
+
+export const getUserData = async (request: Request, response: Response) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM UserSettings WHERE userid = $1",
+      [request.params.id],
+    );
+
+    console.log("Update user data request sent");
+    response.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json("Failed to fetch user data");
+  }
+};
+
+export const addPoster = async (request: Request, response: Response) => {
+  try {
+    await pool.query(
+      "INSERT INTO Poster (id, club_id, user_id, location, position, img_path) VALUES ($1, $2, $3, $4, $5, $6)",
+      [
+        request.body.id,
+        request.body.clubId,
+        request.body.userId,
+        request.body.location,
+        request.body.position,
+        request.body.imgPath,
+      ],
+    );
+
+    response.status(200);
+  } catch {
+    response.status(500).json("Failed to add poster data");
+  }
+};
+
+export const updateUserSettings = async (_request: Request, response: Response) => {
+  // No need to do anything since there is nothing in the user settings table
+  // other than the user ID.
+  response.status(200);
+};
+
+export const deleteUserData = async (request: Request, response: Response) => {
+  try {
+    await pool.query("DELETE FROM UserSettings WHERE userid = $1", [
+      request.params.id,
+    ]);
+
+    console.log("Delete user data request processed");
+    response.status(200);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json("Failed to delete user data");
+  }
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,75 +1,15 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 
-import pool from "../libs/db";
+import {
+  addPoster,
+  deleteUserData,
+  getUserData,
+  listUsers,
+  updateUserSettings,
+} from "../controllers/users/userController";
 
 const router = Router();
 export default router;
-
-const listUsers = async (_request: Request, response: Response) => {
-  try {
-    const result = await pool.query("SELECT * FROM Users");
-    console.log("Get list of users from database");
-    response.status(200).json(result.rows);
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: "Failed to list users" });
-  }
-};
-
-const getUserData = async (request: Request, response: Response) => {
-  try {
-    const result = await pool.query(
-      "SELECT * FROM UserSettings WHERE userid = $1",
-      [request.params.id],
-    );
-
-    console.log("Update user data request sent");
-    response.status(200).json(result.rows);
-  } catch (error) {
-    console.error(error);
-    response.status(500).json("Failed to fetch user data");
-  }
-};
-
-const addPoster = async (request: Request, response: Response) => {
-  try {
-    await pool.query(
-      "INSERT INTO Poster (id, club_id, user_id, location, position, img_path) VALUES ($1, $2, $3, $4, $5, $6)",
-      [
-        request.body.id,
-        request.body.clubId,
-        request.body.userId,
-        request.body.location,
-        request.body.position,
-        request.body.imgPath,
-      ],
-    );
-
-    response.status(200);
-  } catch {
-    response.status(500).json("Failed to add poster data");
-  }
-};
-
-const updateUserSettings = async (_request: Request, response: Response) => {
-  // No need to do anything since there is nothing in the user settings table
-  // other than the user ID.
-  response.status(200);
-};
-
-const deleteUserData = async (request: Request, response: Response) => {
-  try {
-    await pool.query("DELETE FROM UserSettings WHERE userid = $1", [
-      request.params.id,
-    ]);
-
-    console.log("Delete user data request processed");
-    response.status(200);
-  } catch (error) {
-    console.error(error);
-    response.status(500).json("Failed to delete user data");
-  }
-};
 
 router.get("/user", listUsers);
 router.get("/user/:id", getUserData);
