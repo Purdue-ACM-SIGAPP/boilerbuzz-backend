07c82bdbb1d2981dbf0c6ccd2d170460c7be51ce
PreethamYerragudi <preetham.yerragudi@gmail.com>
2025-09-30 20:52:42 -0400

    Finished CRUD functions for User Settings Table

--- src/routes/userSettingRoutes.ts
+++ src/routes/userSettingRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import {
-  getUserSettings
+  getUserSettings,
+  getUserSettingsByUserId,
+  createUserSetting,
+  deleteUserSetting,
+  // putUserSetting
 } from "@/controllers/userSettings/userSettingsController";
 
 const userSettingsRouter = Router();
 
 userSettingsRouter.get("/user/settings", getUserSettings);
-
+userSettingsRouter.get("/user/settings/:id", getUserSettingsByUserId);
+userSettingsRouter.post("/user/settings", createUserSetting);
+// userSettingsRouter.put("/user/settings/:id", putUserSetting)
+userSettingsRouter.delete("/user/settings/:id", deleteUserSetting)
 export default userSettingsRouter;
