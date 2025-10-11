44b52e3c5b7cafaffb517ef1a7b7967041e65be1
zhang <zhangmarvin595@gmail.com>
2025-10-11 14:56:02 -0400

    Added Primary Key for userposterrelation

--- src/scripts/userposterrelation.sql
+++ src/scripts/userposterrelation.sql
@@ -1,4 +1,5 @@
 CREATE TABLE user_poster (
   user_id integer REFERENCES appuser (id),
-  poster_id integer REFERENCES poster (id)
+  poster_id integer REFERENCES poster (id),
+  PRIMARY KEY (user_id, poster_id)
 )
