a74e9cce8de0444afd701f19d90a8d40586f6216
Tyler16 <tpautler@purdue.edu>
2025-10-16 19:52:38 -0400

    Added missing semicolons

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware({ secretKey: _config.CLERK_SECRET_KEY }));
 
 const PORT = _config.APP_PORT || 3000;
+
+// INFO: For swagger dev purposes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/protected', clerkMiddleware(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req);
+  if (!userId) {
+     return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  // Use Clerk's JavaScript Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(userId);
+
+  return res.json({ user })
+});
+
+app.use("/api", clubRouter);
+app.use("/api", userClubRouter);
+app.use("/api", userSettingsRouter)
+
 app.get("/api", (_req, res) => {// Test route
   res.status(200).json({ message: "Hello from the server,public endpoint " });
 });
