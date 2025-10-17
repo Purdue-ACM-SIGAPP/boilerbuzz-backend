2b72a09c27b64f48fea564e802736c5755ab06a9
zhang <zhangmarvin595@gmail.com>
2025-10-16 20:13:11 -0400

    Split user poster relationship into two tables: one for likes and one for bookmarks

--- src/scripts/userposterlike.sql
+++ src/scripts/userposterlike.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_poster (
+CREATE TABLE user_poster_like (
   user_id integer REFERENCES appuser (id),
   poster_id integer REFERENCES poster (id),
   PRIMARY KEY (user_id, poster_id)
