5efd6cf9f816e9464dd676b3c758095ab71617b8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2025-10-21 20:28:49 -0400

    Club API documentation

--- src/controllers/clubs/clubsController.ts
+++ src/controllers/clubs/clubsController.ts
@@ -3,6 +3,11 @@ import { Request, Response } from "express";
 import pool from "@/libs/db";
 
 const getClubs = async (_req: Request, res: Response) => {
+  /*
+    #swagger.summary = "List All Clubs"
+    #swagger.description = "Retrieves all club IDs and club permissions"
+  */
+
   try {
     const data = await pool.query('SELECT * FROM club');
     res.status(200).json(data.rows);
@@ -18,6 +23,14 @@ const getClubs = async (_req: Request, res: Response) => {
 };
 
 const getClub = async (_req: Request, res: Response) => {
+  /*
+    #swagger.summary = "Get Club"
+    #swagger.description = "Retrieves club ID and permission for specified club"
+    #swagger.parameters["id"] = {
+      description: "Club ID"
+    }
+  */
+
   try {
     const data = await pool.query('SELECT * FROM Club WHERE id = $1', [_req.params.id]);
     res.status(200).json(data.rows[0]);
@@ -33,6 +46,16 @@ const getClub = async (_req: Request, res: Response) => {
 };
 
 const addClub = async (_req: Request, res: Response) => {
+  /*
+    #swagger.summary = "Add Club"
+    #swagger.description = "Creates a new club with specified permissions"
+    #swagger.body["member_post_permissions"] = {
+      in: "body",
+      description: "Post permission for members",
+      type: "boolean"
+    }
+  */
+
   try {
     const data = await pool.query('INSERT INTO Club (member_post_permissions) VALUES ($1) RETURNING *', [_req.body.member_post_permissions]);
     res.status(200).json(data.rows);
@@ -48,6 +71,18 @@ const addClub = async (_req: Request, res: Response) => {
 };
 
 const updateClub = async (_req: Request, res: Response) => {
+  /*
+    #swagger.summary = "Update Club"
+    #swagger.description = "Updates club member post permissions"
+    #swagger.parameters["id"] = {
+      description: "Club ID"
+    }
+    #swagger.body["member_post_permissions"] = {
+      description: "Post permission for members",
+      type: "boolean"
+    }
+  */
+
   try {
     const data = await pool.query('UPDATE Club SET member_post_permissions = $1 WHERE id = $2', [_req.body.member_post_permissions, _req.params.id]);
     res.status(200).json(data.rows);
@@ -64,6 +99,14 @@ const updateClub = async (_req: Request, res: Response) => {
 };
 
 const deleteClub = async (_req: Request, res: Response) => {
+  /*
+    #swagger.summary = "Delete Club"
+    #swagger.description = "Deletes a club"
+    #swagger.parameters["id"] = {
+      description: "Club ID"
+    }
+  */
+
   try {
     const data = await pool.query('DELETE FROM Club WHERE id = $1', [_req.params.id]);
     res.status(200).json(data.rows);
